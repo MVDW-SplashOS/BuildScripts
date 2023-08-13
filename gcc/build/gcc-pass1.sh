38f72e68a3269b833dd63fd2b55e18c94f23f085
MVDW <maxvdwolf@gmail.com>
2023-08-13 16:51:19 +0200

    Update gcc-pass1.sh

--- gcc/build/gcc-pass1.sh
+++ gcc/build/gcc-pass1.sh
@@ -1,10 +1,10 @@
 
-tar -xf "../mpfr-${config_tools_list__mpfr__version}.tar.xz"
-mv -vn "mpfr-${config_tools_list__mpfr__version}" mpfr
-tar -xf "../gmp-${config_tools_list__gmp__version}.tar.xz"
-mv -vn "gmp-${config_tools_list__gmp__version}" gmp
-tar -xf "../mpc-${config_tools_list__mpc__version}.tar.gz"
-mv -vn "mpc-${config_tools_list__mpc__version}" mpc
+tar -xf "../mpfr-4.2.0.tar.xz"
+mv -vn "mpfr-4.2.0" mpfr
+tar -xf "../gmp-6.2.1.tar.xz"
+mv -vn "gmp-6.2.1" gmp
+tar -xf "../mpc-1.3.1.tar.gz"
+mv -vn "mpc-1.3.1" mpc
 
 
 sed -e '/m64=/s/lib64/lib/' \
