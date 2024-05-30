ac637c8fac2974eeeda2c555b4ae4720003f005d
MVDW <maxvdwolf@gmail.com>
2024-05-30 22:22:42 +0200

    add debug messages to tcl package

--- tcl/8.6.13/build/tcl-pass1.sh
+++ tcl/8.6.13/build/tcl-pass1.sh
@@ -5,6 +5,8 @@ cd unix
 
 make
 
+echo "SPLASH_TCL_DEBUG: 1"
+
 sed -e "s|$SRCDIR/unix|/usr/lib|" \
     -e "s|$SRCDIR|/usr/include|"  \
     -i tclConfig.sh
@@ -22,17 +24,34 @@ sed -e "s|$SRCDIR/unix/pkgs/itcl4.2.3|/usr/lib/itcl4.2.3|" \
 
 unset SRCDIR
 
-make test
+echo "SPLASH_TCL_DEBUG: 2"
+
 make install
+
+echo "SPLASH_TCL_DEBUG: 3"
 
 chmod -v u+w /usr/lib/libtcl8.6.so
 
+echo "SPLASH_TCL_DEBUG: 4"
+
 make install-private-headers
 
+echo "SPLASH_TCL_DEBUG: 5"
+
 ln -sfv tclsh8.6 /usr/bin/tclsh
+
+echo "SPLASH_TCL_DEBUG: 6"
+
 mv /usr/share/man/man3/{Thread,Tcl_Thread}.3
+
+echo "SPLASH_TCL_DEBUG: 7"
 
 cd ..
 tar -xf ../tcl_docs-8.6.13.tar.xz --strip-components=1
+
+echo "SPLASH_TCL_DEBUG: 8"
+
 mkdir -v -p /usr/share/doc/tcl-8.6.13
 cp -v -r  ./html/* /usr/share/doc/tcl-8.6.13
+
+echo "SPLASH_TCL_DEBUG: 9"
